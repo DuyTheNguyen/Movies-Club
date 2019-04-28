f437c59a8761ea1ea47af657511b797b42bbaa74
The Deligent Dave <duy27295@gmail.com>
2019-04-28 19:05:06 +1000

    update UI add navigation case for main page add css

---
+++ DProject/DProject-war/src/java/web/NavigationManagedBean.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web;
+
+import java.io.Serializable;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author 101036886
+ */
+@Named(value = "navigationManagedBean")
+@RequestScoped
+public class NavigationManagedBean implements Serializable {
+
+    /**
+     * Creates a new instance of NavigationManagedBean
+     */
+    public NavigationManagedBean() {
+    }
+    
+    public String MainMenuNavigation(String destination){
+        String result = "error";
+        switch(destination){
+        case "your_account":
+            result = goToYourAccount();
+            break;
+        case "your_ticket":
+            result = goToYourTicket();
+            break;
+        case "list_of_showtime":
+            result = goToListOfShowtime();
+            break;
+        default:
+             result = goToErrorPage();
+            break;
+        }
+        
+        return result;
+    }
+    
+    private String goToYourAccount(){
+        return "your_account";
+    }
+    
+    private String goToYourTicket(){
+        return "your_ticket";
+    }
+    
+    private String goToListOfShowtime(){
+        return "list_of_showtime";
+    }
+    
+    private String goToErrorPage(){
+        return "error";
+    }
+}
